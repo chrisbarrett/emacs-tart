04659d6bd48ed6da5ea86394b3859d38de6d0c73
Chris Barrett <chris@walrus.cool>
2026-02-05 21:41:08 +1300

    Add nix-community cachix for pre-built Emacs

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
